4351931693764fc95809eac67b601e8aea094902
munene <munene@kali.munene>
2025-03-19 15:25:12 +0300

    Navbar js for menu responsiveness

--- src/main.js
+++ src/main.js
@@ -98,3 +98,12 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.querySelector("[data-collapse-toggle='navbar-sticky']");
+  const navbarMenu = document.getElementById("navbar-sticky");
+
+  toggleButton.addEventListener("click", function () {
+    navbarMenu.classList.toggle("hidden");
+  });
+});
